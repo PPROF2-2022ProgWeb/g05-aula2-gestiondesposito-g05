12b867ca375e87c19dd11b13b3658d1b6c2f418e
Gaston Ferreyra <ferreyragaston351@gmail.com>
2022-06-21 03:30:36 +0100

    Carga Angular

---
+++ Angular/store/src/app/models/item.ts
@@ -0,0 +1,7 @@
+export class Items{
+id:number=0;
+title:string='';
+price:number=0;
+quantity:number=0;
+completed:boolean=false;
+}
---
+++ Angular/store/src/app/shared/components/contenedor1/contenedor1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenedor1',
+  templateUrl: './contenedor1.component.html',
+  styleUrls: ['./contenedor1.component.scss']
+})
+export class Contenedor1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
