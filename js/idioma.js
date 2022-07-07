87f60410e9fc7f0e425947fc10157485fc818e2a
mario <mario.gonzalez@hotmail.com.ar>
2022-07-06 22:58:12 -0300

    Index

--- js/idioma.js
+++ js/idioma.js
@@ -3,7 +3,7 @@ function idioma_espanol() {
     cambia_idioma();
     x = document.querySelectorAll(".esp");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "block";
+      x[i].style.display = "flex";
     }
 }
 /*-----------------------------------------------------------------------------------*/
@@ -11,22 +11,13 @@ function idioma_ingles() {
     cambia_idioma();
     x = document.querySelectorAll(".eng");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "block";
+      x[i].style.display = "flex";
     }
 }
-/*-----------------------------------------------------------------------------------*/
-function idioma_portugues() {
-    cambia_idioma();
-    x = document.querySelectorAll(".port");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "block";
-    }
-}
-
 /*-----------------------------------------------------------------------------------*/
 function cambia_idioma() {
     let x, j;
-    x = document.querySelectorAll(".esp, .eng, .port"); 
+    x = document.querySelectorAll(".esp, .eng"); 
     for (j = 0; j < x.length; j++) {
       x[j].style.display = "none";
     }
