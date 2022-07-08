3aeba70f704d4fec021573ef78f76c4e5d7eab47
Gaston Ferreyra <ferreyragaston351@gmail.com>
2022-07-07 23:40:42 -0300

    multilenguaje #32

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { Cuerpo1Component } from './shared/components/cuerpo1/cuerpo1.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +20,24 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//AOT Sopoerte de compilacion
+
+export function httpTranslateLoader(http:HttpClient) {
+  return new TranslateHttpLoader(http);
+
+}
