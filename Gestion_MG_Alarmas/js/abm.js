3ebc7449106aa5d46ee80e0f269f4ba780582fbe
Mario <mario.gonzalez@hotmail.com.ar>
2022-07-14 21:23:52 -0300

    vs

--- Gestion_MG_Alarmas/js/abm.js
+++ Gestion_MG_Alarmas/js/abm.js
@@ -2,7 +2,7 @@
  function alta() {
     borra();
 
-    x = document.querySelectorAll(".alta");
+    x = document.querySelectorAll(".alta, .pestana_alta");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "inline-flex";
     }
@@ -14,7 +14,6 @@
 
     }
 
-
     x = document.querySelectorAll(".alta_b");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "block";
@@ -25,7 +24,7 @@
  function modificar() {
     borra();
 
-    x = document.querySelectorAll(".modif");
+    x = document.querySelectorAll(".modif, .pestana_alta");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "inline-flex";
     }
@@ -46,7 +45,7 @@
 /*-----------------------------------------------------------------------------------*/
 function baja() {
     borra();
-    x = document.querySelectorAll(".baja");
+    x = document.querySelectorAll(".baja, .pestana_alta");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "inline-flex";
     }
@@ -67,7 +66,7 @@ function baja() {
 /*-----------------------------------------------------------------------------------*/
 function consulta() {
   borra();
-  x = document.querySelectorAll(".tabla1, .contenedor_tabla");
+  x = document.querySelectorAll(".contenedor_tabla, .tabla1");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "inline-flex";
   }
@@ -82,7 +81,7 @@ function consulta() {
 
 function borra() {
     let x, j;
-    x = document.querySelectorAll(".alta, .modif, .baja, .tabla1, .graba, .contenedor_tabla"); 
+    x = document.querySelectorAll(".alta, .modif, .baja, .tabla1, .graba, .contenedor_tabla, .pestana_alta"); 
     for (j = 0; j < x.length; j++) {
       x[j].style.display = "none";
       x[j].value = "";
@@ -94,5 +93,19 @@ function borra() {
     }
   }
   /*-----------------------------------------------------------------------------------*/
+
+
+
+  var mediaqueryList = window.matchMedia("(max-width: 576px)");
+  mediaqueryList.addListener(function (EventoMediaQueryList) {
+
+    x = document.querySelectorAll(".pestana_alta, .contenedor_tabla");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+
+    
+
+  });
 
  
