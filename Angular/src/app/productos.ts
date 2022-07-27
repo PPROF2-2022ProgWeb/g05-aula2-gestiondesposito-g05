471cde09199835e8961500eaa9f66911a842cb36
Gaston Ferreyra <ferreyragaston351@gmail.com>
2022-07-27 04:16:05 -0300

    productos

---
+++ Angular/src/app/productos.ts
@@ -0,0 +1,102 @@
+export const productos=[
+    {
+        id:1,
+        nombre:'producto1',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:2,
+        nombre:'producto2',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:3,
+        nombre:'producto3',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:4,
+        nombre:'producto4',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:5,
+        nombre:'producto5',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:6,
+        nombre:'producto6',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:7,
+        nombre:'producto7',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:8,
+        nombre:'producto8',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:9,
+        nombre:'producto9',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    },
+    {
+        id:10,
+        nombre:'producto10',
+        precio:100,
+        descripcion:'descripcion',
+        imagen:'',
+        cantidad:100,
+        disponible:true
+
+    }
+];
--- Angular/src/app/shared/components/tienda/tienda.component.ts
+++ Angular/src/app/shared/components/tienda/tienda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {productos} from '../../../productos';
 
 @Component({
   selector: 'app-tienda',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiendaComponent implements OnInit {
 
+  productos=productos;
   constructor() { }
 
   ngOnInit(): void {
