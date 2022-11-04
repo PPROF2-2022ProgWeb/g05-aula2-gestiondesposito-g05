6f901e61677c6bbea44abaac3b1fd0a50ce8df0f
gastonloco <ferreyragaston351@gmail.com>
2022-11-04 18:34:14 -0300

    _model

--- Frontend Projec/src/app/add-new-product/add-new-product.component.ts
+++ Frontend Projec/src/app/add-new-product/add-new-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../_model/product.model';
 @Component({
   selector: 'app-add-new-product',
   templateUrl: './add-new-product.component.html',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewProductComponent implements OnInit {
 
+  product: Product={
+    productName:"",
+    productDescription:"",
+    productDiscountedPrice: 0,
+    productActualPrice: 0,
+  }
   constructor() { }
 
   ngOnInit(): void {
