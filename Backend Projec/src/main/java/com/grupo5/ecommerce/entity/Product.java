5fdd646e44bcfa40a8a9823ff26115080d13922d
gastonloco <ferreyragaston351@gmail.com>
2022-11-04 20:08:33 -0300

    Relaciones producto e imagen

--- Backend Projec/src/main/java/com/grupo5/ecommerce/entity/Product.java
+++ Backend Projec/src/main/java/com/grupo5/ecommerce/entity/Product.java
@@ -1,9 +1,7 @@
 package com.grupo5.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Product {
@@ -13,9 +11,19 @@ public class Product {
     private Integer productId;
     private String productName;
     private String productDescription;
-
     private Double productDiscountedPrice;
     private Double productActualPrice;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "product_images",
+    joinColumns = {
+         @JoinColumn(name = "product_id")
+    },
+            inverseJoinColumns={
+                    @JoinColumn(name = "image_id")
+            }
+    )
+    private Set<ImageModel> productImages;
 
     public Integer getProductId() {
         return productId;
