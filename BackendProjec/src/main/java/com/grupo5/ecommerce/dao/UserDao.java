0c1e55395bcd3481fa8ebeeb3dba63f28428c6ee
gastonloco <ferreyragaston351@gmail.com>
2022-11-04 17:25:28 -0300

    elminar registro dañado

--- BackendProjec/src/main/java/com/grupo5/ecommerce/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.grupo5.ecommerce.dao;
-
-import com.grupo5.ecommerce.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
